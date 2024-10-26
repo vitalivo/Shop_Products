05f118225c33895ac30531a2f4f441e52c7008cc
vitalivo <Vitalivo@gmail.com>
2024-10-26 15:19:50 +0300

    Первый коммит

---
+++ simpleapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import (
+    ProductsList, ProductDetail, ProductCreate, ProductUpdate, ProductDelete
+)
+
+urlpatterns = [
+    path('', ProductsList.as_view(), name='product_list'),
+    path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
+    path('create/', ProductCreate.as_view(), name='create'),
+    path('<int:pk>/update/', ProductUpdate.as_view(), name='product_update'),
+    path('<int:pk>/delete/', ProductDelete.as_view(), name='product_delete'),
+]
---
+++ simpleapp/views.py
@@ -0,0 +1,54 @@
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .filters import ProductFilter
+from .forms import ProductForm
+from .models import Product
+
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products.html'
+    context_object_name = 'products'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
+
+
+class ProductCreate(LoginRequiredMixin, CreateView):
+    raise_exception = True
+    form_class = ProductForm
+    model = Product
+    template_name = 'create.html'
+
+
+class ProductUpdate(UpdateView):
+
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('product_list')
+
+
